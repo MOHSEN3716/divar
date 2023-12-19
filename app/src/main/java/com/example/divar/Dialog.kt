eeff5e2931c471078778e974155ba6e78b6fbba1
sana center <MOHSEN_3716_MOHSEN@GMAIL.COM>
2023-12-19 18:17:18 +0330

    dialog full

--- app/src/main/java/com/example/divar/Dialog.kt
+++ app/src/main/java/com/example/divar/Dialog.kt
@@ -4,14 +4,30 @@ import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
 
 class Dialog : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dialog)
 
+        showdialog()
+    }
+        fun showdialog(){
+            var dialog=Dialog(this)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.setContentView(R.layout.custom_dialog)
+            var yes =dialog.findViewById<Button>(R.id.btnno)
+            yes.setOnClickListener{
 
-
+                dialog.dismiss()
+            }
+            dialog.show()
+        val window=dialog.window
+            window?.setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT)
 
         }
-    }
+
+}
